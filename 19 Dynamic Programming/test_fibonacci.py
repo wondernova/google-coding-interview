1e0c194c74dcf1d846ea2267d26c8dba95e126f5
Anderson <a141890@gmail.com>
2018-09-28 01:49:32 +0900

    Add fibonacci with memoize

--- 19 Dynamic Programming/test_fibonacci.py
+++ 19 Dynamic Programming/test_fibonacci.py
@@ -1,4 +1,5 @@
 def test_fibonacci_with_dp():
+    # Dynamic Programming
     assert 0 == fib(0)
     assert 1 == fib(1)
     assert 1 == fib(2)
@@ -10,6 +11,19 @@ def test_fibonacci_with_dp():
     assert 21 == fib(8)
     assert 34 == fib(9)
     assert 55 == fib(10)
+
+    # Recursion with Memoize
+    assert 0 == fib_recursive(0)
+    assert 1 == fib_recursive(1)
+    assert 1 == fib_recursive(2)
+    assert 2 == fib_recursive(3)
+    assert 3 == fib_recursive(4)
+    assert 5 == fib_recursive(5)
+    assert 8 == fib_recursive(6)
+    assert 13 == fib_recursive(7)
+    assert 21 == fib_recursive(8)
+    assert 34 == fib_recursive(9)
+    assert 55 == fib_recursive(10)
 
 
 def fib(n):
@@ -23,3 +37,19 @@ def fib(n):
         a, b = b, sum
 
     return sum
+
+
+def fib_recursive(n):
+    assert n >= 0
+    if n <= 1:
+        return n
+
+    if not hasattr(fib_recursive, 'memoize'):
+        fib_recursive.memoize = dict()
+
+    if n in fib_recursive.memoize:
+        return fib_recursive.memoize[n]
+
+    result = fib_recursive(n - 1) + fib_recursive(n - 2)
+    fib_recursive.memoize[n] = result
+    return result
