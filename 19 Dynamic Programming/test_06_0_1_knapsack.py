178381c331fe10f12b4547fcaff43bbae4222e99
Anderson <a141890@gmail.com>
2018-10-01 21:33:53 +0900

    Add repeatable knapsack

--- 19 Dynamic Programming/test_06_0_1_knapsack.py
+++ 19 Dynamic Programming/test_06_0_1_knapsack.py
@@ -11,31 +11,99 @@ def test_01knapsack():
     v = values
     answer format: (maximum value, [weights...])
     """
+
+    # 0/1 Recursive Knapsack with weights
     w = [1, 2, 4, 2, 5]
     v = [5, 3, 5, 3, 2]
     assert (16, [2, 4, 2, 1]) == knapsack_resursive(w, v, 10)
 
     w = [3, 2, 4, 1, 3, 2]
     v = [1, 2, 3, 1, 4, 3]
-    assert (11, [2, 3, 1, 4]) == knapsack_resursive(w, v, 8)
+    assert (10, [2, 3, 1, 2]) == knapsack_resursive(w, v, 8)
 
     w = [2, 1, 7, 5, 3, 4]
     v = [6, 0, 5, 2, 2, 4]
-    assert (13, [3, 7, 2]) == knapsack_resursive(w, v, 11)
+    assert (12, [4, 3, 1, 2]) == knapsack_resursive(w, v, 11)
+
+    w = [2, 1, 4, 3, 1, 4]
+    v = [2, 2, 9, 0, 3, 5]
+    assert (19, [4, 1, 4, 1]) == knapsack_resursive(w, v, 11)
+
+    # 0/1 Recursive Knapsack with maximum value
+    w = [1, 2, 4, 2, 5]
+    v = [5, 3, 5, 3, 2]
+    assert 16 == knapsack_max_value(w, v, 10, len(w))
+
+    w = [3, 2, 4, 1, 3, 2]
+    v = [1, 2, 3, 1, 4, 3]
+    assert 10 == knapsack_max_value(w, v, 8, len(w))
+
+    w = [2, 1, 7, 5, 3, 4]
+    v = [6, 0, 5, 2, 2, 4]
+    assert 12 == knapsack_max_value(w, v, 11, len(w))
+
+    w = [2, 1, 4, 3, 1, 4]
+    v = [2, 2, 9, 0, 3, 5]
+    assert 19 == knapsack_max_value(w, v, 11, len(w))
+
+    # 0/1 Knapsack with repeatable values
+    w = [1, 2, 4, 2, 5]
+    v = [3, 0, 0, 1, 0]
+    assert [12, [1, 1, 1, 1]] == knapsack_repeat(w, v, 4)
+
+    w = [1, 3, 4, 2, 5]
+    v = [1, 4, 4, 2, 3]
+    assert [14, [2, 3, 3, 3]] == knapsack_repeat(w, v, 11)
+
+    w = [1, 3, 4, 2, 5]
+    v = [0, 4, 5, 3, 6]
+    assert [10, [2, 2, 3]] == knapsack_repeat(w, v, 7)
+
+    w = [1, 3, 4, 2, 5]
+    v = [1, 4, 7, 3, 8]
+    assert [15, [5, 4]] == knapsack_repeat(w, v, 9)
 
 
 def knapsack_resursive(w, v, capacity):
     def f(capacity, i, sack=[]):
         if i <= 0:
             return [0, sack]
-        elif capacity <= 0:
-            return [0, sack]
+        elif w[i - 1] > capacity:
+            return f(capacity, i - 1, sack)
 
         r1 = f(capacity - w[i - 1], i - 1, sack + [w[i - 1]])
-        r1[0] = v[i - 1] + r1[0]
+        r1[0] += v[i - 1]
         r2 = f(capacity, i - 1, sack)
 
         return max(r1, r2, key=lambda x: x[0])
 
     max_value, weights = f(capacity, len(w))
     return max_value, weights
+
+
+def knapsack_max_value(w, v, capacity, i):
+    if i <= 0:
+        return 0
+    elif w[i - 1] > capacity:
+        return knapsack_max_value(w, v, capacity, i - 1)
+
+    return max(v[i - 1] + knapsack_max_value(w, v, capacity - w[i - 1], i - 1),
+               knapsack_max_value(w, v, capacity, i - 1))
+
+
+def knapsack_repeat(w, v, capacity):
+    def f(capacity, i, sack=[]):
+        if i <= 0:
+            return [0, sack]
+        elif w[i - 1] > capacity:
+            return f(capacity, i - 1, sack)
+
+        r1 = f(capacity - w[i - 1], i - 1, sack + [w[i - 1]])
+        r1[0] += v[i - 1]
+        r2 = f(capacity - w[i - 1], i, sack + [w[i - 1]])
+        r2[0] += v[i - 1]
+        r3 = f(capacity, i - 1, sack)
+
+        return max(r1, r2, r3, key=lambda x: x[0])
+
+    return f(capacity, len(w))
