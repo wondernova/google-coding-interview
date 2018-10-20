0710e1b419b598e85d7f97901cb1f2fe77b44c69
Anderson <a141890@gmail.com>
2018-10-20 22:34:21 +0900

    Fix all ancestors

--- 07 Binary Tree/test_05_print_all_ancestors.py
+++ 07 Binary Tree/test_05_print_all_ancestors.py
@@ -27,7 +27,7 @@ def test_print_all_ancestors():
     assert [1, 2, 5] == get_ancestors(node, 5)
 
     node = create_tree([1, 2, 3, None, 5, 6, 7])
-    assert [] == get_ancestors(node, 17)
+    assert None == get_ancestors(node, 17)
 
     node = create_tree([100, None, 5, None, None, 6, 7])
     assert [100, 5] == get_ancestors(node, 5)
@@ -36,7 +36,7 @@ def test_print_all_ancestors():
     assert [100, 5, 7] == get_ancestors(node, 7)
 
     node = create_tree([100, None, 5, None, None, 6, 7])
-    assert [] == get_ancestors(node, 0)
+    assert None == get_ancestors(node, 0)
 
     node = create_tree([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
     assert [10, 9, 6, 0] == get_ancestors(node, 0)
@@ -57,10 +57,10 @@ def test_print_all_ancestors():
     assert [10, 8, 5] == get_ancestors(node, 5)
 
     node = create_tree([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-    assert [] == get_ancestors(node, -1)
+    assert None == get_ancestors(node, -1)
 
     node = create_tree([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-    assert [] == get_ancestors(node, 11)
+    assert None == get_ancestors(node, 11)
 
     node = create_tree([1, 2, 3, None, None, None, 7])
     assert [1, 3, 7] == get_ancestors(node, 7)
@@ -73,16 +73,34 @@ def test_print_all_ancestors():
     node = create_tree([5, 2, 10, None, 17, 6, 22, None, None, None,
                         None, None, None, None, 8, None, None, None, None, None,
                         None, None, None, None, None, None, None, None, None, 9])
-    assert [] == get_ancestors(node, 15)
+    assert None == get_ancestors(node, 15)
 
     node = create_tree([])
-    assert [] == get_ancestors(node, 0)
+    assert None == get_ancestors(node, 0)
+
+
+def get_ancestors(root, target):
+    if not root:
+        return None
+
+    if root.value == target:
+        l = [root.value]
+        return l
+
+    left = get_ancestors(root.left, target)
+    right = get_ancestors(root.right, target)
+
+    l = left or right
+    if l:
+        l.insert(0, root.value)
+
+    return l
 
 
 from collections import deque
 
 
-def get_ancestors(root, target):
+def get_ancestors2(root, target):
     ancestors = deque()
 
     def f(root):
